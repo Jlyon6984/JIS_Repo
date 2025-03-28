32b7c0eaf9449abb84520db4c442c9bd3e3215da
Jake <Jlyon26@wooster.edu>
2025-03-28 10:54:10 -0400

    Version 1.0, user input and hashing of input

--- JIS_Main.py
+++ JIS_Main.py
@@ -1,4 +1,44 @@
-# ##Welcome to My JIS main!
+# Welcome to My JIS main!
 # Author: Jacob Lyon
 # Project: User Password hashing and cracking via John the Ripper.
 
+import hashlib
+import matplotlib
+import bcrypt
+
+
+def Hashing(user_input):
+    # This section of the function makes use of the MD5 algorithm via hashlib to hash user input
+    # The result of this hash is also reported back to the user.
+    print("Hashing with MD5")
+    MD5_Hash = hashlib.md5(user_input.encode()).hexdigest()
+    print("Result: " + MD5_Hash, "\n" )
+
+    # This section of the function makes use of the SHA-256 algorithm via hashlib to hash user input
+    # The result of this hash is also reported back to the user.
+    print("Hashing with SHA-256")
+    SHA256_Hash = hashlib.sha256(user_input.encode()).hexdigest()
+    print("Result: " + SHA256_Hash,"\n")
+
+    # This section of the function makes use of the Bcrypt algorithm via the Bcrypt Library to hash user
+    # input The result of this hash is also reported back to the user.
+    print("Hashing with Bcrypt")
+    Bcrypt_Hash = bcrypt.hashpw(user_input.encode(), bcrypt.gensalt())
+    print("Result: ",Bcrypt_Hash)
+
+    return 0
+
+
+
+
+def main():
+    print("Welcome to the Password Cracker! Please Input a Password-String: ")
+    user_input = input()
+
+
+    print("Hashing of Password will now commence!\n")
+    Hashing(user_input)
+
+    return 0
+
+main()
