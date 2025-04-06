a6fd0cc677db7771273866294f0cb9fc03bb95ca
Jake <Jlyon26@wooster.edu>
2025-04-06 16:39:35 -0400

    Version 1.2, Additon of subproccess that run John as well as return basic results of crack, only Wordlist mode is supported rn to be added on to in next development. also splits hashes into unique files.

--- JIS_Main.py
+++ JIS_Main.py
@@ -2,9 +2,13 @@
 # Author: Jacob Lyon
 # Project: User Password hashing and cracking via John the Ripper.
 
+import bcrypt
 import hashlib
 import matplotlib
-import bcrypt
+import os
+import subprocess
+import time
+
 
 
 def Hashing(user_input):
@@ -28,20 +32,68 @@ def Hashing(user_input):
 
     return MD5_Hash,SHA256_Hash,Bcrypt_Hash
 
+
+
+
+#Helper function that writes the three hashed results to a .txt file
+#File will serve as input into John the Ripper
 def File_Write(MD5,SHA,Bcrypt):
-    file = open("Passwords.txt","w")
-    list = []
+    md5_file = "md5_hashes.txt"
+    sha_file = "sha256_hashes.txt"
+    bcrypt_file = "bcrypt_hashes.txt"
 
-    list.append(MD5 + "\n")
+    with open(md5_file,"w") as MD5_Files:
+        MD5_Files.write(MD5 + "\n")
 
-    list.append(SHA + "\n")
+    with open(sha_file,"w") as Sha256_Files:
+        Sha256_Files.write(SHA + "\n")
 
-    list.append(Bcrypt + "\n")
+    with open(bcrypt_file,"w") as Bcrypt_Files:
+        Bcrypt_Files.write(Bcrypt + "\n")
 
-    file.writelines(list)
-    file.close()
-    print("Hashes Saved to File!")
 
+
+    print("Hashes Saved to Files!")
+    return md5_file, sha_file, bcrypt_file
+
+
+
+
+def Run_JtR(File_of_Hash, Hash_Format, Wordlist, timeout=30):
+    print("Attemping a crack on", Hash_Format)
+    start_time = time.time()
+
+    try:
+        subprocess.run(["john", f"--wordlist={Wordlist}", f"--format={Hash_Format}",
+                        File_of_Hash], check = True, timeout = timeout)
+    except subprocess.TimeoutExpired:
+        print("John ran longer than {timeout} seconds")
+
+    except subprocess.CalledProcessError as e:
+        print("Error Running JtR")
+
+
+def JtR_results(File_of_Hash,Format):
+    result = subprocess.run(
+        ["john", "--show",f"--format={Format}", File_of_Hash],
+        capture_output=True,
+        text=True
+    )
+    print(result)
+
+    cracked_output = result.stdout.strip().splitlines()
+
+    # Check if any line contains the question mark (?)
+    cracked = any('?' in line for line in cracked_output)
+
+
+    if cracked:
+        print("Cracked: True")
+        # Extract and print the cracked password from the line (before the space and ?)
+        cracked_password = next(line for line in cracked_output if '?' in line).split()[0]
+        print("Cracked Password:", cracked_password)
+    else:
+        print("Cracked: False")
 
 
 
@@ -53,9 +105,26 @@ def main():
     print("Hashing of Password will now commence!\n")
     MD5_Hash, SHA256_Hash, Bcrypt_Hash = Hashing(user_input)
 
-    print("The Hashes of the password will now be saved to a file!")
-    File_Write(SHA256_Hash,MD5_Hash,Bcrypt_Hash)
+    print("\n The Hashes of the password will now be saved to a file!")
+    md5_file, sha_file, bcrypt_file = File_Write(MD5_Hash,SHA256_Hash,Bcrypt_Hash)
 
-    return 0
+    wordlist = "/Users/jakelyon/Desktop/rockyou.txt"
 
-main()
+    Run_JtR(md5_file, "raw-md5", wordlist, timeout=30)
+    JtR_results(md5_file,"raw-md5")
+
+    #Run_JtR(sha_file, "raw-sha256", wordlist, timeout=30)
+
+    #Run_JtR(bcrypt_file, "bcrypt", wordlist, timeout=30)s
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
